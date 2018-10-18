db6d308d44335245cfb0fdc79b7c5fc281f93322
ksenia <ksenia.timofeeva@gmail.com>
2018-10-18 12:51:12 +0300

    HomeWork #1 - comments are corrected, private fields are added

--- src/test/java/hw1/HomePageContentSuite.java
+++ src/test/java/hw1/HomePageContentSuite.java
@@ -15,12 +15,26 @@ import static org.testng.Assert.assertTrue;
 
 public class HomePageContentSuite {
 
+    private List<String> headerElementsText = Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS");
+
+    private List<String> imagesText = Arrays.asList("To include good practices\nand ideas from successful\nEPAM project",
+            "To be flexible and\ncustomizable",
+            "To be multiplatform",
+            "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
+
+    private String mainText = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, " +
+            "SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. " +
+            "UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS " +
+            "NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN " +
+            "REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.";
+
+
     @Test
     public void homePageContentTest() {
 
         setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
-        //0 Open BR
+        //0 Open Browser
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
@@ -28,7 +42,7 @@ public class HomePageContentSuite {
         //1 Open test site
         driver.navigate().to("https://epam.github.io/JDI/");
 
-        //2 Assert title
+        //2 Assert Browser title
         assertEquals(driver.getTitle(), "Home Page");
 
         //3 Login
@@ -37,25 +51,23 @@ public class HomePageContentSuite {
         driver.findElement(By.cssSelector("[id = 'Password']")).sendKeys("1234");
         driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
 
-        //4 Assert username
+        //4 Assert User name that user is loggined
         assertEquals(driver.findElement(By.cssSelector(".profile-photo [ui = 'label']")).getText(), "PITER CHAILOVSKII");
 
-        //5 Assert title
+        //5 Assert Browser title
         assertEquals(driver.getTitle(), "Home Page");
 
-        //6 Assert 4 items on the header section
+        //6 Assert that 4 items on the header section are displayed and they have proper texts
         List<WebElement> headerElements = driver.findElements(By.cssSelector("ul.uui-navigation.nav > li > a"));
 
         assertEquals(headerElements.size(), 4);
-
-        List<String> headerElementsText = Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS");
 
         for (WebElement element : headerElements) {
             assertTrue(element.isDisplayed());
             assertTrue(headerElementsText.contains(element.getText()));
         }
 
-        //7 Assert 4 images are displayed
+        //7 Assert that there are 4 images on the Index Page and they are displayed
         List<WebElement> images = driver.findElements(By.cssSelector("span.icons-benefit"));
 
         assertEquals(images.size(), 4);
@@ -64,12 +76,10 @@ public class HomePageContentSuite {
             assertTrue(image.isDisplayed());
         }
 
-        //8 Assert text under 4 images
+        //8 Assert that there are 4 texts on the Index Page under icons and they have proper text
         List<WebElement> textUnderImages = driver.findElements(By.cssSelector("div.benefit-icon+span"));
-        List<String> imagesText = Arrays.asList("To include good practices\nand ideas from successful\nEPAM project",
-                "To be flexible and\ncustomizable",
-                "To be multiplatform",
-                "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
+
+        assertEquals(textUnderImages.size(),4);
 
         for (WebElement text : textUnderImages) {
             assertTrue(text.isDisplayed());
@@ -77,34 +87,27 @@ public class HomePageContentSuite {
         }
 
         //9 Assert text of the main header
-        String mainText = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, " +
-                "SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. " +
-                "UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS " +
-                "NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN " +
-                "REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.";
-
         assertEquals(driver.findElement(By.cssSelector("h3.main-title")).getText(), "EPAM FRAMEWORK WISHES…");
         assertEquals(driver.findElement(By.cssSelector("p.main-txt")).getText(), mainText);
 
-        //10 Assert iframe exists
+        //10 Assert iframe
         WebElement iframe = driver.findElement(By.cssSelector("iframe"));
         assertTrue(iframe.isDisplayed());
 
-        //11 Assert logo in iframe
+        //11 Switch to the iframe and check that there is Epam logo
         driver.switchTo().frame(iframe);
         assertTrue(driver.findElement(By.cssSelector("[id = epam_logo]")).isDisplayed());
 
         //12 Switch to original window
         driver.switchTo().defaultContent();
 
-        //13 Assert sub header
+        //13 Assert a text of the sub header
         assertEquals(driver.findElement(By.cssSelector("div.main-content > h3 > a")).getText(), "JDI GITHUB");
 
-        //14 Assert link
+        //14 Assert that JDI GITHUB is a link and has a proper URL
         WebElement subHeaderLink = driver.findElement(By.linkText("JDI GITHUB"));
         assertTrue(subHeaderLink.isDisplayed());
         assertEquals(subHeaderLink.getAttribute("href"), "https://github.com/epam/JDI");
-
 
         //15 Assert left section
         assertTrue(driver.findElement(By.cssSelector("div.uui-side-bar")).isDisplayed());
@@ -112,7 +115,7 @@ public class HomePageContentSuite {
         //16 Assert footer
         assertTrue(driver.findElement(By.cssSelector("footer")).isDisplayed());
 
-        //17 Close BR
+        //17 Close Browser
         driver.close();
     }
 }
--- src/test/java/hw1/HomePageContentSuiteSoft.java
+++ src/test/java/hw1/HomePageContentSuiteSoft.java
@@ -14,13 +14,27 @@ import static java.lang.System.setProperty;
 
 public class HomePageContentSuiteSoft {
 
+    private List<String> headerElementsText = Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS");
+
+    private List<String> imagesText = Arrays.asList("To include good practices\nand ideas from successful\nEPAM project",
+            "To be flexible and\ncustomizable",
+            "To be multiplatform",
+            "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
+
+    private String mainText = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, " +
+            "SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. " +
+            "UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS " +
+            "NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN " +
+            "REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.";
+
+
     @Test
     public void homePageContentTest() {
 
         setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         SoftAssert softAssert = new SoftAssert();
 
-        //0 Open BR
+        //0 Open Browser
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
@@ -28,7 +42,7 @@ public class HomePageContentSuiteSoft {
         //1 Open test site
         driver.navigate().to("https://epam.github.io/JDI/");
 
-        //2 Assert title
+        //2 Assert Browser title
         softAssert.assertEquals(driver.getTitle(), "Home Page");
 
         //3 Login
@@ -37,25 +51,23 @@ public class HomePageContentSuiteSoft {
         driver.findElement(By.cssSelector("[id = 'Password']")).sendKeys("1234");
         driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
 
-        //4 Assert username
+        //4 Assert User name that user is loggined
         softAssert.assertEquals(driver.findElement(By.cssSelector(".profile-photo [ui = 'label']")).getText(), "PITER CHAILOVSKII");
 
-        //5 Assert title
+        //5 Assert Browser title
         softAssert.assertEquals(driver.getTitle(), "Home Page");
 
-        //6 Assert 4 items on the header section
+        //6 Assert that 4 items on the header section are displayed and they have proper texts
         List<WebElement> headerElements = driver.findElements(By.cssSelector("ul.uui-navigation.nav > li > a"));
 
         softAssert.assertEquals(headerElements.size(), 4);
-
-        List<String> headerElementsText = Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS");
 
         for (WebElement element : headerElements) {
             softAssert.assertTrue(element.isDisplayed());
             softAssert.assertTrue(headerElementsText.contains(element.getText()));
         }
 
-        //7 Assert 4 images are displayed
+        //7 Assert that there are 4 images on the Index Page and they are displayed
         List<WebElement> images = driver.findElements(By.cssSelector("span.icons-benefit"));
 
         softAssert.assertEquals(images.size(), 4);
@@ -64,12 +76,10 @@ public class HomePageContentSuiteSoft {
             softAssert.assertTrue(image.isDisplayed());
         }
 
-        //8 Assert text under 4 images
+        //8 Assert that there are 4 texts on the Index Page under icons and they have proper text
         List<WebElement> textUnderImages = driver.findElements(By.cssSelector("div.benefit-icon+span"));
-        List<String> imagesText = Arrays.asList("To include good practices\nand ideas from successful\nEPAM project",
-                "To be flexible and\ncustomizable",
-                "To be multiplatform",
-                "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…");
+
+        softAssert.assertEquals(textUnderImages.size(),4);
 
         for (WebElement text : textUnderImages) {
             softAssert.assertTrue(text.isDisplayed());
@@ -77,34 +87,27 @@ public class HomePageContentSuiteSoft {
         }
 
         //9 Assert text of the main header
-        String mainText = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, " +
-                "SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. " +
-                "UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS " +
-                "NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN " +
-                "REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.";
-
         softAssert.assertEquals(driver.findElement(By.cssSelector("h3.main-title")).getText(), "EPAM FRAMEWORK WISHES…");
         softAssert.assertEquals(driver.findElement(By.cssSelector("p.main-txt")).getText(), mainText);
 
-        //10 Assert iframe exists
+        //10 Assert iframe
         WebElement iframe = driver.findElement(By.cssSelector("iframe"));
         softAssert.assertTrue(iframe.isDisplayed());
 
-        //11 Assert logo in iframe
+        //11 Switch to the iframe and check that there is Epam logo
         driver.switchTo().frame(iframe);
         softAssert.assertTrue(driver.findElement(By.cssSelector("[id = epam_logo]")).isDisplayed());
 
         //12 Switch to original window
         driver.switchTo().defaultContent();
 
-        //13 Assert sub header
+        //13 Assert a text of the sub header
         softAssert.assertEquals(driver.findElement(By.cssSelector("div.main-content > h3 > a")).getText(), "JDI GITHUB");
 
-        //14 Assert link
+        //14 Assert that JDI GITHUB is a link and has a proper URL
         WebElement subHeaderLink = driver.findElement(By.linkText("JDI GITHUB"));
         softAssert.assertTrue(subHeaderLink.isDisplayed());
         softAssert.assertEquals(subHeaderLink.getAttribute("href"), "https://github.com/epam/JDI");
-
 
         //15 Assert left section
         softAssert.assertTrue(driver.findElement(By.cssSelector("div.uui-side-bar")).isDisplayed());
@@ -112,7 +115,7 @@ public class HomePageContentSuiteSoft {
         //16 Assert footer
         softAssert.assertTrue(driver.findElement(By.cssSelector("footer")).isDisplayed());
 
-        //17 Close BR
+        //17 Close Browser
         driver.close();
     }
 }
